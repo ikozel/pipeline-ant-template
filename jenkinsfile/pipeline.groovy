13fdad476cac4897334eda4778725175b563f36c
Igor Kozel <igorkozel@Igors-MacBook-Pro.local>
2017-07-02 12:12:30 -0700

    tasks

--- jenkinsfile/pipeline.groovy
+++ jenkinsfile/pipeline.groovy
@@ -1,8 +1,14 @@
 node {
-        stage('Checkout') {
-                checkout scm
-        }
-        stage('Build') {
-                sh 'pwd'
-        }
+  stage('Checkout') {
+        checkout scm
+  }
+  stage('Build') {
+        sh '~/workspace/apache-ant-1.10.1/bin/ant clean compile'
+  }
+  stage('Test') {
+        sh '~/workspace/apache-ant-1.10.1/bin/ant test'
+  }
+  stage('Package') {
+        sh '~/workspace/apache-ant-1.10.1/bin/ant package'
+  }
 }
